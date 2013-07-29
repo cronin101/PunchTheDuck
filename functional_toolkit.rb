39332c235db186acdfbbc43930d67041f6f1f4eb
Aaron Cronin <cronin@live.co.uk>
2013-07-29 23:25:23 +0100

    Early ideas

---
+++ functional_toolkit.rb
@@ -0,0 +1,50 @@
+# Ruby Functional Toolkit. Unsensible hacks to make the language ridiculous.
+
+# Attach a lambda to an object as a method.
+class Object
+
+  class FunctionPrototype < Struct.new(:klass, :method_name)
+
+    def call(instance, *args)
+      raise ArgumentError unless instance.is_a? klass
+      instance.send method_name, *args
+    end
+
+    def set(function)
+      raise ArgumentError unless function.respond_to? :call
+      klass.send :define_method, method_name, &function
+    end
+
+  end
+
+  def prototype(name)
+    FunctionPrototype.new(self, name)
+  end
+
+end
+
+# Duck-punch Proc to auto-curry when too few arguments are supplied
+class Proc
+  alias_method :__noncurry_call__, :call
+  private :__noncurry_call__
+
+  def call(*args)
+    begin
+      __noncurry_call__(*args)
+    rescue ArgumentError
+      curry[args]
+    end
+  end
+
+end
+
+# Duck-punch Enumerable to not complain when partially mapping via .to_proc shortcut
+module Enumerable
+  alias_method :__noncurry_map__, :map
+  private :__noncurry_map__
+
+  def map(&proc)
+    __noncurry_map__(&proc.curry)
+  end
+
+end
---
+++ spec/functional_toolkit_spec.rb
@@ -0,0 +1,53 @@
+require './functional_toolkit'
+
+describe Proc do
+
+  it "Should not break tradional proc-calling" do
+    ->(string) { string.upcase }.('bang!').should == 'BANG!'
+  end
+
+  it "Automatically curries if too few arguments are supplied" do
+    two_argument_proc = ->(arg_one, arg_two){:result}
+    two_argument_proc.call(:one_argument).should be_an_instance_of(Proc)
+
+    two_argument_proc.call(:one_argument).call(:another_argument).should == :result
+  end
+
+  it "Allows partial mapping via auto-currying" do
+    two_argument_proc = ->(one, two){:result}
+    (1..10)
+      .map { |num| two_argument_proc.(:two) }
+      .map { |func| func.(:two) }
+      .should == ([:result] * 10)
+
+    (1..10)
+      .map(&two_argument_proc)
+      .map { |func| func.(:two) }
+      .should == ([:result] * 10)
+
+    (1..10)
+      .map { |num, other_param| num + other_param }
+      .map { |func| func.call(1) }
+      .should == (2..11).to_a
+  end
+
+  it "Should not break traditional mapping" do
+    (1..5).map { |num| ('x' + num.to_s).to_sym }.should == [:x1, :x2, :x3, :x4, :x5]
+  end
+
+end
+
+describe Object do
+  it "Should be possible to set instance methods of a class via prototype" do
+    String::prototype(:shoutcase).set ->() { self.upcase + '!' }
+    "hello".shoutcase.should == 'HELLO!'
+
+    String::prototype(:lotsa_shouts).set ->(times) { ([self.shoutcase, ' '] * times).join[0..-2] }
+    "hey".lotsa_shouts(3).should == "HEY! HEY! HEY!"
+  end
+
+  it "Should be possible to call instance methods from a class prototype" do
+    String::prototype(:to_sym).('test').should == :test
+  end
+
+end
